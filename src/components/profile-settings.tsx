a6d106d27a999153e8e9ee6b5f6934167a8a849b
Daniel Marques <daniel.brz2009@gmail.com>
2023-04-09 20:53:43 -0300

    WIP

--- src/components/profile-settings.tsx
+++ src/components/profile-settings.tsx
@@ -1,5 +1,3 @@
-'use client'
-
 import {
   NavigationMenu,
   NavigationMenuContent,
@@ -7,51 +5,11 @@ import {
   NavigationMenuLink,
   NavigationMenuList,
   NavigationMenuTrigger,
-  navigationMenuTriggerStyle,
 } from '@/components/ui/navigation-menu'
 import { cn } from '@/utils/cn'
-import { useSession } from 'next-auth/react'
+import { signIn, signOut, useSession } from 'next-auth/react'
 import Image from 'next/image'
-import Link from 'next/link'
 import * as React from 'react'
-
-const components: { title: string; href: string; description: string }[] = [
-  {
-    title: 'Alert Dialog',
-    href: '/docs/primitives/alert-dialog',
-    description:
-      'A modal dialog that interrupts the user with important content and expects a response.',
-  },
-  {
-    title: 'Hover Card',
-    href: '/docs/primitives/hover-card',
-    description:
-      'For sighted users to preview content available behind a link.',
-  },
-  {
-    title: 'Progress',
-    href: '/docs/primitives/progress',
-    description:
-      'Displays an indicator showing the completion progress of a task, typically displayed as a progress bar.',
-  },
-  {
-    title: 'Scroll-area',
-    href: '/docs/primitives/scroll-area',
-    description: 'Visually or semantically separates content.',
-  },
-  {
-    title: 'Tabs',
-    href: '/docs/primitives/tabs',
-    description:
-      'A set of layered sections of content—known as tab panels—that are displayed one at a time.',
-  },
-  {
-    title: 'Tooltip',
-    href: '/docs/primitives/tooltip',
-    description:
-      'A popup that displays information related to an element when the element receives keyboard focus or the mouse hovers over it.',
-  },
-]
 
 export const ProfileSettings = () => {
   const session = useSession()
@@ -69,35 +27,18 @@ export const ProfileSettings = () => {
               alt='user pic'
             />
           </NavigationMenuTrigger>
+
           <NavigationMenuContent>
-            <ul className='grid gap-3 p-6 md:w-[400px] lg:w-[500px] lg:grid-cols-[.75fr_1fr]'>
-              <li className='row-span-3'>
-                <NavigationMenuLink asChild>
-                  <Link
-                    className='flex h-full w-full select-none flex-col
-                    justify-end rounded-md bg-gradient-to-b from-rose-500
-                    to-indigo-700 p-6 no-underline outline-none focus:shadow-md'
-                    href='/'
-                  >
-                    <div className='mt-4 mb-2 text-lg font-medium text-white'>
-                      shadcn/ui
-                    </div>
-                    <p className='text-sm leading-tight text-white/90'>
-                      Beautifully designed components built with Radix UI and
-                      Tailwind CSS.
-                    </p>
-                  </Link>
-                </NavigationMenuLink>
-              </li>
-              <ListItem href='/docs' title='Introduction'>
-                Re-usable components built using Radix UI and Tailwind CSS.
-              </ListItem>
-              <ListItem href='/docs/installation' title='Installation'>
-                How to install dependencies and structure your app.
-              </ListItem>
-              <ListItem href='/docs/primitives/typography' title='Typography'>
-                Styles for headings, paragraphs, lists...etc
-              </ListItem>
+            <ul className='flex flex-col p-6'>
+              {session.status === 'authenticated' ? (
+                <ListItem onClick={() => signOut()}>Sign out</ListItem>
+              ) : (
+                <ListItem
+                  onClick={() => signIn('google', { callbackUrl: '/' })}
+                >
+                  Sign in
+                </ListItem>
+              )}
             </ul>
           </NavigationMenuContent>
         </NavigationMenuItem>
